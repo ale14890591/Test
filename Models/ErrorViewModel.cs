661a02ecd8ad842a195fd4d820846b2856839df7
Alexander <ale14890591@yandex.ru>
2019-07-21 20:10:53 +0300

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace testmvcgit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
